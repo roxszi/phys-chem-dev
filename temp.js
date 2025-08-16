5846dfd87ac2d0ed1773e80e43a6ed5b36669c14
SI_Cheng-yun <sichengyun@163.com>
2025-08-16 20:45:33 +0800

    250816a markdown的加粗符号对中文标点兼容较差，因此宜将中文标点放在加粗符号外面

--- temp.js
+++ temp.js
@@ -2,10 +2,6 @@
 
 // const a = Math.cos(60 * Math.PI / 180)
 
-const theta = Math.atan2(45, 45)
-
-console.log("theta: ", theta)
-
-const a = Math.tan(theta)
-
-console.log("a: ", a)
+const res = await fetch("https://atomgit.com/cpuer/yaoda-admin")
+const data = await res.text()
+console.log("data: ", data)
