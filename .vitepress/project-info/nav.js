7a8338acaabc9b0b8cab33be8e3a944ed62ff86a
SI_Cheng-Yun <sichengyun@163.com>
2025-09-30 15:48:15 +0800

    250930d 优化导航栏内容

--- .vitepress/project-info/nav.js
+++ .vitepress/project-info/nav.js
@@ -8,20 +8,26 @@ export default {
 
   /** 默认语言，即中文 @type { import("vitepress").DefaultTheme.NavItem[] } */
   root: [
-    { text: "Global Server", link: "https://roxszi.github.io/phys-chem/en/" },
-    // { text: "技术栈", items: [
-    //   { text: "VitePress", link: "https://vitepress.dev/zh/" },
-    //   { text: "TDsign", link: "https://tdesign.tencent.com/" }
-    // ]},
+    { text: "节点加速", items: [
+      { text: "国内访问", link: "https://cpuer.atomgit.net/phys-chem/" },
+      { text: "海外访问", link: "https://roxszi.github.io/phys-chem/" }
+    ]},
+    { text: "技术栈", items: [
+      { text: "VitePress", link: "https://vitepress.dev/zh/" },
+      { text: "TDsign", link: "https://tdesign.tencent.com/" }
+    ]}
   ],
 
   /** 英文 @type { import("vitepress").DefaultTheme.NavItem[] } */
   en: [
-    { text: "中国大陆访问", link: "https://cpuer.atomgit.net/phys-chem/" },
-    // { text: "Tech Stack", items: [
-    //   { text: "VitePress", link: "https://vitepress.dev/" },
-    //   { text: "TDsign", link: "https://tdesign.tencent.com/index-en" }
-    // ]},
+    { text: "Optimize Nodes", items: [
+      { text: "China Access", link: "https://cpuer.atomgit.net/phys-chem/en/" },
+      { text: "Global Access", link: "https://roxszi.github.io/phys-chem/en/" }
+    ]},
+    { text: "Tech Stack", items: [
+      { text: "VitePress", link: "https://vitepress.dev/" },
+      { text: "TDsign", link: "https://tdesign.tencent.com/index-en" }
+    ]}
   ],
 
 }
